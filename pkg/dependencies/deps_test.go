a6f292dcb2f6321dc3a1fd6b82673381aa8c5629
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2022-12-12 18:23:51 +0100

    tests: Use `CMAKE` instead of `CLANG` in deps tests
    The `CLANG` dependency has special handling that makes it unsuitable for
    mocking the `RunfilesFinder`, leading to local test failures that aren't
    reproducible in CI.

--- pkg/dependencies/deps_test.go
+++ pkg/dependencies/deps_test.go
@@ -11,78 +11,78 @@ import (
 )
 
 func TestCheck(t *testing.T) {
-	keys := []Key{CLANG}
+	keys := []Key{CMAKE}
 	deps := getDeps(keys)
 
-	dep := deps[CLANG]
+	dep := deps[CMAKE]
 	dep.GetVersion = func(d *Dependency) (*semver.Version, error) {
 		return &d.MinVersion, nil
 	}
 
 	finder := &mocks.RunfilesFinderMock{}
-	finder.On("ClangPath").Return("clang", nil)
+	finder.On("CMakePath").Return("cmake", nil)
 
 	err := check(keys, deps, finder)
 	require.NoError(t, err)
 }
 
 func TestCheck_NotInstalled(t *testing.T) {
-	keys := []Key{CLANG}
+	keys := []Key{CMAKE}
 	deps := getDeps(keys)
 
 	finder := &mocks.RunfilesFinderMock{}
-	finder.On("ClangPath").Return("", errors.New("missing-error"))
+	finder.On("CMakePath").Return("", errors.New("missing-error"))
 
 	err := check(keys, deps, finder)
 	require.Error(t, err)
 }
 
 func TestCheck_WrongVersion(t *testing.T) {
-	keys := []Key{CLANG}
+	keys := []Key{CMAKE}
 	deps := getDeps(keys)
 
 	// overwrite GetVersion for clang
-	dep := deps[CLANG]
+	dep := deps[CMAKE]
 	dep.GetVersion = func(d *Dependency) (*semver.Version, error) {
 		return semver.MustParse("1.0.0"), nil
 	}
 
 	finder := &mocks.RunfilesFinderMock{}
-	finder.On("ClangPath").Return("clang", nil)
+	finder.On("CMakePath").Return("cmake", nil)
 
 	err := check(keys, deps, finder)
 	require.Error(t, err)
 }
 
 func TestCheck_ShortVersion(t *testing.T) {
-	keys := []Key{CLANG}
+	keys := []Key{CMAKE}
 	deps := getDeps(keys)
 
 	// overwrite GetVersion for clang
-	dep := deps[CLANG]
+	dep := deps[CMAKE]
 	dep.GetVersion = func(d *Dependency) (*semver.Version, error) {
-		return semver.MustParse("11.0"), nil
+		return semver.MustParse("3.16"), nil
 	}
 
 	finder := &mocks.RunfilesFinderMock{}
-	finder.On("ClangPath").Return("clang", nil)
+	finder.On("CMakePath").Return("cmake", nil)
 
 	err := check(keys, deps, finder)
 	require.NoError(t, err)
 }
 
 func TestCheck_UnableToGetVersion(t *testing.T) {
-	keys := []Key{CLANG}
+	keys := []Key{CMAKE}
 	deps := getDeps(keys)
 
 	// overwrite GetVersion for clang
-	dep := deps[CLANG]
+	dep := deps[CMAKE]
 	dep.GetVersion = func(d *Dependency) (*semver.Version, error) {
 		return nil, errors.New("version-error")
 	}
 
 	finder := &mocks.RunfilesFinderMock{}
-	finder.On("ClangPath").Return("clang", nil)
+	finder.On("CMakePath").Return("cmake", nil)
 
 	err := check(keys, deps, finder)
 	require.NoError(t, err)
