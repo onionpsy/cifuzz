onionpsy/cifuzz

commit a628f3031c84c27337facff8de3f67f71b725b4d

fileutil: Remove CanonicalPath and its usages
Resolving the path of the project directory and all files and
directories in is causing problems, for example LCOV reports contain
unresolved source file paths, which we can't make relative to the
resolved project directory.

We found that the correct solution is to not resolve symlinks in paths.
The reason we did that in the first place was to make files and
directories from the host available in the minijail sandbox, which
should not be a problem anymore since we now make the root filesystem
of the host available as a read-only mount in the sandbox.

Removing this function requires adding calls to `filepath.Abs` in some
places in the `other` build system's build package.

internal/build/other/other.go
@@ -163,19 +163,12 @@ func (b *Builder) Build(fuzzTest string) (*build.Result, error) {
 
 	// For the build system type "other", we expect the default seed corpus next
 	// to the fuzzer executable.
-	seedCorpus, err := fileutil.CanonicalPath(executable + "_inputs")
-	if err != nil {
-		return nil, err
-	}
+	seedCorpus := executable + "_inputs"
 	runtimeDeps, err := b.findSharedLibraries(fuzzTest)
 	if err != nil {
 		return nil, err
 	}
 	wd, err := os.Getwd()
-	if err != nil {
-		return nil, err
-	}
-	buildDir, err := fileutil.CanonicalPath(wd)
 	if err != nil {
 		return nil, err
 	}
@@ -185,7 +178,7 @@ func (b *Builder) Build(fuzzTest string) (*build.Result, error) {
 		Executable:      executable,
 		GeneratedCorpus: generatedCorpus,
 		SeedCorpus:      seedCorpus,
-		BuildDir:        buildDir,
+		BuildDir:        wd,
 		ProjectDir:      b.ProjectDir,
 		Engine:          b.Engine,
 		Sanitizers:      b.Sanitizers,
@@ -313,7 +306,11 @@ func (b *Builder) setCoverageEnv() error {
 
 func (b *Builder) findFuzzTestExecutable(fuzzTest string) (string, error) {
 	if exists, _ := fileutil.Exists(fuzzTest); exists {
-		return fileutil.CanonicalPath(fuzzTest)
+		absPath, err := filepath.Abs(fuzzTest)
+		if err != nil {
+			return "", errors.WithStack(err)
+		}
+		return absPath, nil
 	}
 
 	var executable string
@@ -345,7 +342,11 @@ func (b *Builder) findFuzzTestExecutable(fuzzTest string) (string, error) {
 	if executable == "" {
 		return "", nil
 	}
-	return fileutil.CanonicalPath(executable)
+	absPath, err := filepath.Abs(fuzzTest)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	return absPath, nil
 }
 
 var sharedLibraryRegex = regexp.MustCompile(`^.+\.((so)|(dylib))(\.\d\w*)*$`)
@@ -372,11 +373,11 @@ func (b *Builder) findSharedLibraries(fuzzTest string) ([]string, error) {
 			return nil
 		}
 		if sharedLibraryRegex.MatchString(info.Name()) {
-			canonicalPath, err := fileutil.CanonicalPath(path)
+			absPath, err := filepath.Abs(path)
 			if err != nil {
-				return err
+				return errors.WithStack(err)
 			}
-			sharedObjects = append(sharedObjects, canonicalPath)
+			sharedObjects = append(sharedObjects, absPath)
 		}
 		return nil
 	})

internal/build/cmake/cmake.go
@@ -154,13 +154,8 @@ func (b *Builder) Configure() error {
 // Build builds the specified fuzz tests with CMake. The fuzz tests must
 // not contain duplicates.
 func (b *Builder) Build(fuzzTests []string) ([]*build.Result, error) {
-	buildDir, err := fileutil.CanonicalPath(b.BuildDir())
-	if err != nil {
-		return nil, err
-	}
-
 	flags := append([]string{
-		"--build", buildDir,
+		"--build", b.BuildDir(),
 		"--config", cmakeBuildConfiguration,
 		"--target"}, fuzzTests...)
 
@@ -178,7 +173,7 @@ func (b *Builder) Build(fuzzTests []string) ([]*build.Result, error) {
 	cmd.Stderr = b.Stderr
 	cmd.Env = b.env
 	log.Debugf("Command: %s", cmd.String())
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		// It's expected that cmake might fail due to user configuration,
 		// so we print the error without the stack trace.
@@ -210,7 +205,7 @@ func (b *Builder) Build(fuzzTests []string) ([]*build.Result, error) {
 			Executable:      executable,
 			GeneratedCorpus: generatedCorpus,
 			SeedCorpus:      seedCorpus,
-			BuildDir:        buildDir,
+			BuildDir:        b.BuildDir(),
 			ProjectDir:      b.ProjectDir,
 			Engine:          b.Engine,
 			Sanitizers:      b.Sanitizers,
@@ -300,10 +295,6 @@ func (b *Builder) getRuntimeDeps(fuzzTest string) ([]string, error) {
 		}
 		status := statusAndDep[:endOfStatus]
 		dep := statusAndDep[endOfStatus+1:]
-		dep, err = fileutil.CanonicalPath(dep)
-		if err != nil {
-			return nil, err
-		}
 
 		switch status {
 		case "UNRESOLVED":
@@ -352,7 +343,7 @@ func (b *Builder) readInfoFileAsPath(fuzzTest string, kind string) (string, erro
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
-	return fileutil.CanonicalPath(string(content))
+	return string(content), nil
 }
 
 func (b *Builder) fuzzTestsInfoDir() (string, error) {

internal/config/config.go
@@ -192,10 +192,5 @@ func FindConfigDir() (string, error) {
 		}
 	}
 
-	dir, err = fileutil.CanonicalPath(dir)
-	if err != nil {
-		return "", errors.WithStack(err)
-	}
-
 	return dir, nil
 }

util/fileutil/fileutil.go
@@ -102,24 +102,6 @@ func IsBelow(path string, root string) (bool, error) {
 	return rel != ".." && !strings.HasPrefix(rel, filepath.FromSlash("../")), nil
 }
 
-// CanonicalPath converts path to an absolute path and follows all symlinks (if any).
-// If path doesn't exist, no parent symlinks are followed.
-func CanonicalPath(path string) (string, error) {
-	absPath, err := filepath.Abs(path)
-	if err != nil {
-		return "", errors.WithStack(err)
-	}
-	canonicalPath, err := filepath.EvalSymlinks(absPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return absPath, nil
-		} else {
-			return "", errors.WithStack(err)
-		}
-	}
-	return canonicalPath, nil
-}
-
 // ForceSymlink creates newname as a symbolic link to oldname. If
 // newname already exists, it will be overwritten.
 func ForceSymlink(oldname, newname string) error {

commit 6be3d15c093a04d765b5755c54ed375052f519bd

build/other: Fix fuzz test executable lookup
a628f3031c84c27337facff8de3f67f71b725b4d regressed the look up of the
fuzz test executable after the build by always using the name of the
fuzz test as a project dir relative path.

internal/build/other/other.go
@@ -341,7 +341,7 @@ func (b *Builder) findFuzzTestExecutable(fuzzTest string) (string, error) {
 	if executable == "" {
 		return "", nil
 	}
-	absPath, err := filepath.Abs(fuzzTest)
+	absPath, err := filepath.Abs(executable)
 	if err != nil {
 		return "", errors.WithStack(err)
 	}

integration-tests/other/testdata/Makefile
@@ -1,24 +1,32 @@
 CXXFLAGS += -std=c++11
 
 clean:
-	rm -rf *.o *.a *.so.* *.so a.out my_fuzz_test
+	rm -rf build *.o *.a *.so.* *.so a.out
 
-libexplore.so.4.2: src/explore/explore_me.cpp src/explore/explore_me.h
-	${CXX} ${CXXFLAGS} -shared -fpic -o libexplore.so.4.2 $<
+build/libexplore.so.4.2: src/explore/explore_me.cpp src/explore/explore_me.h
+	mkdir -p build
+	${CXX} ${CXXFLAGS} -shared -fpic -o build/libexplore.so.4.2 $<
 
-libexplore.so.4: libexplore.so.4.2
-	ln -s libexplore.so.4.2 libexplore.so.4
+build/libexplore.so.4: build/libexplore.so.4.2
+	mkdir -p build
+	ln -s libexplore.so.4.2 build/libexplore.so.4
 
-libexplore.so: libexplore.so.4
-	ln -s libexplore.so.4 libexplore.so
+build/libexplore.so: build/libexplore.so.4
+	mkdir -p build
+	ln -s libexplore.so.4 build/libexplore.so
 
-my_fuzz_test: libexplore.so
+build/my_fuzz_test: build/libexplore.so
 	@echo "Building $@"
+	mkdir -p build
+	cp -r my_fuzz_test_inputs build/
 	# The FUZZ_TEST_CXXFLAGS and FUZZ_TEST_LDFLAGS environment variables
 	# are set by cifuzz when it executes the build command. Those must
 	# be passed to the compiler and linker (compiling and linking is
 	# done in a single invocation here, so we pass both to $CXX here).
-	${CXX} ${CXXFLAGS} ${FUZZ_TEST_CXXFLAGS} ${FUZZ_TEST_LDFLAGS} -o $@ $@.cpp -Wl,-rpath '-Wl,$$ORIGIN' -L. -lexplore
+	${CXX} ${CXXFLAGS} ${FUZZ_TEST_CXXFLAGS} ${FUZZ_TEST_LDFLAGS} -o build/my_fuzz_test my_fuzz_test.cpp -Wl,-rpath '-Wl,$$ORIGIN' -Lbuild -lexplore
+
+.PHONY: my_fuzz_test
+my_fuzz_test: build/my_fuzz_test
 
 crashing_fuzz_test.o: src/coverage/crashing_fuzz_test.c
 	${CC} ${CFLAGS} ${FUZZ_TEST_CFLAGS} -c $<