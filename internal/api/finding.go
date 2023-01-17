fc3ba77cd99fdda07e4443a3fb8cabf91505939f
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-01-17 11:13:32 +0100

    feat(api): fill finding fields with correct info

--- internal/api/finding.go
+++ internal/api/finding.go
@@ -27,28 +27,106 @@ type Finding struct {
 }
 
 type ErrorReport struct {
-	Logs    []string `json:"logs"`
-	Details string   `json:"details"`
+	Logs      []string `json:"logs"`
+	Details   string   `json:"details"`
+	Type      string   `json:"type,omitempty"`
+	InputData []byte   `json:"input_data,omitempty"`
+
+	DebuggingInfo      *DebuggingInfo `json:"debugging_info,omitempty"`
+	HumanReadableInput string         `json:"human_readable_input,omitempty"`
+	MoreDetails        *MoreDetails   `json:"more_details,omitempty"`
+	Tag                string         `json:"tag,omitempty"`
+	ShortDescription   string         `json:"short_description,omitempty"`
+}
+
+type DebuggingInfo struct {
+	ExecutablePath string        `json:"executable_path,omitempty"`
+	RunArguments   []string      `json:"run_arguments,omitempty"`
+	BreakPoints    []BreakPoint  `json:"break_points,omitempty"`
+	Environment    []Environment `json:"environment,omitempty"`
+}
+
+type BreakPoint struct {
+	SourceFilePath string           `json:"source_file_path,omitempty"`
+	Location       *FindingLocation `json:"location,omitempty"`
+	Function       string           `json:"function,omitempty"`
+}
+
+type FindingLocation struct {
+	Line   uint32 `json:"line,omitempty"`
+	Column uint32 `json:"column,omitempty"`
+}
+
+type Environment struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type MoreDetails struct {
+	ID          string    `json:"id,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Severity    *Severity `json:"severity,omitempty"`
+	Language    string    `json:"language,omitempty"`
+}
+
+type Severity struct {
+	Description string  `json:"description,omitempty"`
+	Score       float32 `json:"score,omitempty"`
 }
 
 func (client *APIClient) UploadFinding(project string, fuzzTarget string, campaignRunName string, fuzzingRunName string, finding *finding.Finding, token string) error {
-	f := Finding{
-		Name:        project + "/findings/cifuzz-" + finding.Name,
-		DisplayName: finding.Name,
-		FuzzTarget:  fuzzTarget,
-		FuzzingRun:  fuzzingRunName,
-		CampaignRun: campaignRunName,
-		ErrorReport: ErrorReport{
-			Logs:    finding.Logs,
-			Details: finding.Details,
-		},
-		Timestamp: time.Now().Format(time.RFC3339),
-	}
-	findingBody := &Findings{
-		Findings: []Finding{f},
+	breakPoints := []BreakPoint{}
+	for _, stackFrame := range finding.StackTrace {
+		breakPoints = append(breakPoints, BreakPoint{
+			SourceFilePath: stackFrame.SourceFile,
+			Location: &FindingLocation{
+				Line:   stackFrame.Line,
+				Column: stackFrame.Column,
+			},
+			Function: stackFrame.Function,
+		})
 	}
 
-	body, err := json.Marshal(findingBody)
+	// FIXME: currently we only fill MoreDetails for Java,
+	// because they are not set for other languages
+	moreDetails := MoreDetails{}
+	if finding.MoreDetails != nil {
+		moreDetails = MoreDetails{
+			ID:   finding.MoreDetails.Id,
+			Name: finding.MoreDetails.Name,
+			Severity: &Severity{
+				Score: finding.MoreDetails.Severity.Score,
+			},
+		}
+	}
+
+	findings := &Findings{
+		Findings: []Finding{
+			{
+				Name:        project + "/findings/cifuzz-" + finding.Name,
+				DisplayName: finding.Name,
+				FuzzTarget:  fuzzTarget,
+				FuzzingRun:  fuzzingRunName,
+				CampaignRun: campaignRunName,
+				ErrorReport: ErrorReport{
+					Logs:      finding.Logs,
+					Details:   finding.Details,
+					Type:      string(finding.Type),
+					InputData: finding.InputData,
+					DebuggingInfo: &DebuggingInfo{
+						BreakPoints: breakPoints,
+					},
+					MoreDetails:      &moreDetails,
+					Tag:              fmt.Sprint(finding.Tag),
+					ShortDescription: finding.ShortDescription(),
+				},
+				Timestamp: time.Now().Format(time.RFC3339),
+			},
+		},
+	}
+
+	body, err := json.Marshal(findings)
 	if err != nil {
 		return errors.WithStack(err)
 	}
