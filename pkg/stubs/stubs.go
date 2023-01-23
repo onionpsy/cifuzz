64cd540e9fcbcfa2740c7b4795e4b9f57dabc0ca
wojtek <dziewiecki@code-intelligence.com>
2023-01-23 14:25:05 +0100

    feat(stubs): add stub for Kotlin

--- pkg/stubs/stubs.go
+++ pkg/stubs/stubs.go
@@ -19,6 +19,9 @@ var cppStub []byte
 //go:embed fuzzTest.java.tmpl
 var javaStub []byte
 
+//go:embed fuzzTest.ktl.tmpl
+var kotlinStub []byte
+
 // Create creates a stub based for the given test type
 func Create(path string, testType config.FuzzTestType) error {
 	exists, err := fileutil.Exists(path)
@@ -34,16 +37,32 @@ func Create(path string, testType config.FuzzTestType) error {
 	switch testType {
 	case config.CPP:
 		content = cppStub
-	case config.JAVA:
-		baseName := strings.TrimSuffix(filepath.Base(path), ".java")
-		content = []byte(strings.Replace(string(javaStub), "__CLASS_NAME__", baseName, 1))
+	case config.JAVA, config.KOTLIN:
+		var ext, langInPath, stub string
+		if testType == config.JAVA {
+			ext = ".java"
+			langInPath = "java"
+			stub = string(javaStub)
+		} else if testType == config.KOTLIN {
+			ext = ".kt"
+			langInPath = "kotlin"
+			stub = string(kotlinStub)
+		}
+
+		baseName := strings.TrimSuffix(filepath.Base(path), ext)
+		content = []byte(strings.Replace(stub, "__CLASS_NAME__", baseName, 1))
 
 		// If we have a valid package name we add it to the template
 		// We assume the project has the standard java project structure
 		if filepath.Dir(path) != "" {
-			packagePath := strings.TrimPrefix(filepath.Dir(path), filepath.Join("src", "test", "java")+string(os.PathSeparator))
+			packagePath := strings.TrimPrefix(filepath.Dir(path), filepath.Join("src", "test", langInPath)+string(os.PathSeparator))
 			packagePath = strings.ReplaceAll(packagePath, string(os.PathSeparator), ".")
-			content = []byte(strings.Replace(string(content), "__PACKAGE__", fmt.Sprintf("package %s;", packagePath), 1))
+
+			packageName := fmt.Sprintf("package %s;", packagePath)
+			if testType == config.KOTLIN {
+				strings.TrimSuffix(packageName, ";")
+			}
+			content = []byte(strings.Replace(string(content), "__PACKAGE__", packageName, 1))
 		}
 	}
 
@@ -67,6 +86,10 @@ func FuzzTestFilename(testType config.FuzzTestType) (string, error) {
 		basename = "my_fuzz_test"
 		ext = "cpp"
 		filePattern = "%s_%d.%s"
+	case config.KOTLIN:
+		basename = "MyClassFuzzTest"
+		ext = "kt"
+		filePattern = "%s%d.%s"
 	case config.JAVA:
 		basename = "MyClassFuzzTest"
 		ext = "java"
