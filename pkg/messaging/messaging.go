877c00038eac806321b5b4d3e94c638e8073f364
Niklas Henrich <niklas.henrich@code-intelligence.com>
2023-03-01 14:56:46 +0100

    feat: use different messaging options and use utm parameters

--- pkg/messaging/messaging.go
+++ pkg/messaging/messaging.go
@@ -1,7 +1,7 @@
 package messaging
 
 import (
-	"math/rand"
+	"fmt"
 	"net/url"
 	"os"
 
@@ -9,51 +9,65 @@ import (
 )
 
 func ShowServerConnectionMessage(server string) *url.Values {
-	messageA := "Do you want to persist your findings?\n" + "Authenticate with the CI Fuzz Server " + server + " to get more insights."
-	messageB := `Code Intelligence provides you a full history for all your findings and allows you to postpone work on findings and completely ignore them to keep the output clean and focused.
 
-	With a free authentication you receive detailed information and solution tips for each findings in your console.
-	
-	All your finding data stay only with us at Code Intelligence and will never be shared
-	
-	Do you want to continue with authentication now?`
+	messagesAndParams := []struct {
+		message          string
+		additionalParams url.Values
+	}{
+		{
+			message: fmt.Sprintf(`Do you want to persist your findings?
+Authenticate with the CI Fuzz Server %s to get more insights.`, server),
+			additionalParams: url.Values{
+				"utm_source":   []string{"cli"},
+				"utm_campaign": []string{"login-message"},
+				"utm_term":     []string{"a"}},
+		},
+		{
+			message: `Code Intelligence provides you with a full history for all your findings
+and allows you to postpone work on findings and completely ignore
+them to keep the output clean and focused.
 
-	additionalParams := url.Values{}
-	if ComputeStablePseudoRandomNumber() == 0 {
-		additionalParams.Add("utm_campaign", "A")
-		log.Notef(messageA)
-	} else {
-		additionalParams.Add("utm_campaign", "B")
-		log.Notef(messageB)
+With a free authentication you receive detailed information and solution tips
+for each finding in your console.
+	
+All your finding data stays only with us at Code Intelligence
+and will never be shared.
+	
+Do you want to continue with authentication now?`,
+			additionalParams: url.Values{
+				"utm_source":   []string{"cli"},
+				"utm_campaign": []string{"login-message"},
+				"utm_term":     []string{"b"},
+			},
+		},
 	}
 
-	return &additionalParams
+	messageIndex, err := pickNumberForMessagingIndex(len(messagesAndParams))
+	if err != nil {
+		messageIndex = 0
+	}
+
+	log.Notef(messagesAndParams[messageIndex].message)
+	return &messagesAndParams[messageIndex].additionalParams
 }
 
 // To avoid that a user sees a different message each time
 // we compute a stable "random" number
-func ComputeStablePseudoRandomNumber() int {
+func pickNumberForMessagingIndex(numberOfMessages int) (int, error) {
 	// Path name for the executable
 	path, err := os.Executable()
 
 	if err != nil {
-		// As a fallback, return "true" random number
-		return rand.Intn(2)
+		return 0, err
 	}
 
 	file, err := os.Stat(path)
 	if err != nil {
-		// As a fallback, return "true" random number
-		return rand.Intn(2)
+		return 0, err
 	}
 
 	// We are using minute instead of hour
 	// to avoid a "bias" for people trying
 	// things out late in the evening
-	minute := file.ModTime().Minute()
-	if minute < 30 {
-		return 0
-	} else {
-		return 1
-	}
+	return file.ModTime().Minute() % numberOfMessages, nil
 }
