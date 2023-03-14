311b440217f1fe1b9f43e6023b91f916892b2a25
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-03-14 15:25:39 +0100

    test(run): check for Visual Studio on Windows

--- internal/cmd/run/run_test.go
+++ internal/cmd/run/run_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -39,6 +40,10 @@ func TestFail(t *testing.T) {
 }
 
 func TestClangMissing(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("clang is not needed on windows and will be provided by Visual Studio")
+	}
+
 	dependencies.MockAllDeps(t)
 	// let the clang dep fail
 	dependencies.OverwriteUninstalled(dependencies.GetDep(dependencies.Clang))
@@ -53,11 +58,15 @@ func TestClangMissing(t *testing.T) {
 
 	output, err := io.ReadAll(testOut)
 	require.NoError(t, err)
-	fmt.Fprintf(os.Stderr, string(output))
+	fmt.Fprint(os.Stderr, string(output))
 	assert.Contains(t, string(output), fmt.Sprintf(dependencies.MessageMissing, "clang"))
 }
 
 func TestLlvmSymbolizerVersion(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("llvm-symbolizer is not needed on windows")
+	}
+
 	dependencies.MockAllDeps(t)
 	dep := dependencies.GetDep(dependencies.LLVMSymbolizer)
 	version := dependencies.OverwriteGetVersionWith0(dep)
@@ -72,7 +81,32 @@ func TestLlvmSymbolizerVersion(t *testing.T) {
 
 	output, err := io.ReadAll(testOut)
 	require.NoError(t, err)
-	fmt.Fprintf(os.Stderr, string(output))
+	fmt.Fprint(os.Stderr, string(output))
 	assert.Contains(t, string(output),
 		fmt.Sprintf(dependencies.MessageVersion, "llvm-symbolizer", dep.MinVersion.String(), version))
+}
+
+func TestVisualStudioMissing(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		t.Skip("only needed on windows")
+	}
+
+	dependencies.MockAllDeps(t)
+
+	dep := dependencies.GetDep(dependencies.VisualStudio)
+	version := dependencies.OverwriteGetVersionWith0(dep)
+
+	// clone the example project because this command needs to parse an actual
+	// project config... if there is none it will fail before the dependency check
+	_, cleanup := testutil.BootstrapExampleProjectForTest("run-cmd-test", config.BuildSystemCMake)
+	defer cleanup()
+
+	_, err := cmdutils.ExecuteCommand(t, New(), os.Stdin, "my_fuzz_test")
+	require.Error(t, err)
+
+	output, err := io.ReadAll(testOut)
+	require.NoError(t, err)
+	fmt.Fprint(os.Stderr, string(output))
+	assert.Contains(t, string(output),
+		fmt.Sprintf(dependencies.MessageVersion, "Visual Studio", dep.MinVersion.String(), version))
 }
